0ebc1dc0f3c6c23879317c1208c0895cf78ea36d
Abheekrishna <abheekrishna17@gmail.com>
2023-06-13 20:27:45 +0530

    Finished

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const rainbowBtn = document.getElementById('rainbow');
 const eraserBtn = document.getElementById('eraser');
 const resetBtn = document.getElementById('reset');
 
+
+
 let DEFAULT_WIDTH = 16;
 const DEFAULT_COLOR = '#393939';
 
@@ -12,38 +14,31 @@ canvasDiv.style.display = 'grid';
 canvasDiv.style.gridTemplateColumns = `repeat(${DEFAULT_WIDTH}, 1fr)`;
 canvasDiv.style.gridTemplateRows = `repeat(${DEFAULT_WIDTH}, 1fr)`;
 
+document.addEventListener('DOMContentLoaded', () => {
+    for(i = 0; i < DEFAULT_WIDTH; i++) {
+        for(j = 0; j < DEFAULT_WIDTH; j++) {
+        const gridCell = document.createElement('div');
+
+            defaultBtn.addEventListener('click', () => {
+                gridCell.addEventListener('mousemove', () => {
+                    gridCell.style.backgroundColor = `${DEFAULT_COLOR}`;
+                })
+            })
 
 
-for (i = 0; i < DEFAULT_WIDTH; i++) {
-    for (j = 0; j < DEFAULT_WIDTH; j++) {
-    const div = document.createElement('div');
-    div.id = 'gridCell';
-    canvasDiv.appendChild(div);
-    }
-}
-const grid = document.getElementById('gridCell');
+            eraserBtn.addEventListener('click', () => {
+                defaultBtn.disabled = true;
+                    gridCell.addEventListener('dragover', () => {
+                    gridCell.style.backgroundColor = 'transparent';
+                })
+            })
 
-defaultBtn.addEventListener('click', () => {
-        grid.style.backgroundColor = '#393939';
-})
+            resetBtn.addEventListener('click', () => {
+                gridCell.style.backgroundColor = 'transparent';
+            })
 
 
-//Clear
-resetBtn.addEventListener('click', () => {
-    for (i = 0; i < DEFAULT_WIDTH; i++) {
-        for (j = 0; j < DEFAULT_WIDTH; j++) {
-        const div = document.createElement('div');
-        div.addEventListener('mousemove', () => {
-        })
-        div.style.backgroundColor = 'transparent';
-        canvasDiv.appendChild(div);
+        canvasDiv.appendChild(gridCell);
         }
     }
-})
-
-//Eraser
-eraserBtn.addEventListener('click', () => {
-    div.addEventListener('mousemove', () => {
-        div.style.backgroundColor = '#fff';
-    })
 })
---
+++ main.js
@@ -0,0 +1,83 @@
+let color = 'black';
+let click = false;
+
+document.addEventListener('DOMContentLoaded', () => {
+    createBoard(16)
+
+    document.querySelector("body").addEventListener('click', (e) => {
+        if(e.target.tagName != 'BUTTON') {
+            click = !click;
+            let draw = document.getElementById('message');
+            if (click === true) {
+                draw.innerText = 'Now you can draw';
+            } else {
+                draw.innerText = 'You cannot draw';
+            }
+        }
+    })
+
+    const boardWidthBtn = document.getElementById('width');
+    boardWidthBtn.addEventListener('click', () => {
+        const size = getSize()
+        createBoard(size)
+    })
+})
+
+const createBoard = (size) => {
+    const board = document.querySelector('.canvas');
+
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    const numberOfDiv = size * size;
+
+    for (i = 0; i < numberOfDiv; i++) {
+        const div = document.createElement('div');
+        div.classList.add('gridCell');
+        div.addEventListener('mouseover', colorDiv)
+        board.appendChild(div);
+    }
+}
+
+const getSize = () => {
+    const inputSize = prompt('Enter the size for the canvas.');
+    const errorMsg = document.getElementById('errorMsg');
+    
+    if (inputSize == "") {
+        errorMsg.innerText = 'Please provide a number.';
+    } else if (inputSize < 0 || inputSize > 100) {
+        errorMsg.innerText = 'Please provide number between 1 and 100';
+    } else {
+        errorMsg.innerText = ' ';
+    }
+    return inputSize;
+}
+
+function colorDiv() {
+    if(click) {
+        if(color == 'random') {
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+        } else {
+            this.style.backgroundColor = '#393939';
+        }
+    }
+}
+
+const setColor = (colorChoice) => {
+    color = colorChoice;
+}
+
+const eraser = () => {
+    let divs = document.querySelectorAll('.gridCell');
+    divs.forEach((div) => {
+        div.addEventListener('mouseover', () => {
+            div.style.backgroundColor = 'transparent';
+        })
+    })
+
+}
+
+const reset = () => {
+    let divs = document.querySelectorAll('.gridCell');
+    divs.forEach((div) => div.style.backgroundColor = '#fff')
+}
